0860aa76e9117bf95650ba4f180a7d0bc4bf4e82
ido reshef <idoreshef10@gmail.com>
2023-07-04 10:11:49 +0300

    made a new patch request to update documents in db

--- server.js
+++ server.js
@@ -74,8 +74,22 @@ app.post('/items', async (req,res) => {
         res.status(500).send({error_message:`There was an error adding the item you sent ${error}` });
     }
 })
-app.patch('/items', async (req, res) => {
-    
+app.patch('/items/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+        const filter = {id:id};
+        const update = {$set:{}};
+        if(req.body.name) update.$set.name = req.body.name;
+        if(req.body.amount) update.$set.amount = req.body.amount;
+        await inventory.updateOne(filter, update);
+        const query = {id:id};
+        const cursor = inventory.find(query);
+        const docs = await cursor.toArray();
+        res.status(200).json({message:`the item with id: ${id} has beed succesfully updated`, updated_doc:docs});
+
+    }catch(error){
+        res.status(500).send({error_message: `there was an error updating the document. error: ${error}`});
+    }
 })
 
 app.listen(PORT, () => console.log(`its alive on http:/localhost:${PORT}`));
